35e336797a3ca6d1794b3a75da5f364e822ba3a2
Avni Rao <64287021+avnirao@users.noreply.github.com>
2025-05-04 17:28:52 -0700

    matcha icon

--- matcha-ranking-app/src/pages/Home.jsx
+++ matcha-ranking-app/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import "../App.css";
-import matcha from "../assets/matcha-avni.png";
 import { Link } from 'react-router-dom';
+import matcha from "../assets/matcha-avni.png";
 
 export default function Home() {
   return (
     <div className="page">
-        <img src={matcha} alt="Matcha Icon" className="matcha-icon-top-left" />
+      <img src={matcha} alt="Matcha Icon" className="matcha-icon-top-left" />
       <p className="brand-heading-1">Hi, I’m <em>Avni</em></p>
       <p className="brand-heading-2">
       and I <span className="underline-animated">love</span> matcha.</p>
