0c83c8c96e4a7b34d1ab5c0ac3b43ef07ea2ffcb
Avni Rao <64287021+avnirao@users.noreply.github.com>
2025-05-04 17:25:22 -0700

    mathca icon

--- matcha-ranking-app/src/pages/Home.jsx
+++ matcha-ranking-app/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import "../App.css";
-import matcha from "../assets/matcha.png";
+import matcha from "../assets/matcha-avni.png";
 import { Link } from 'react-router-dom';
 
 export default function Home() {
