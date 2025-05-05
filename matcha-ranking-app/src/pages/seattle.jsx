49e77331adfbb660eca2b902c485460778b51e42
Avni Rao <64287021+avnirao@users.noreply.github.com>
2025-05-04 17:13:27 -0700

    matcha icon

--- matcha-ranking-app/src/pages/seattle.jsx
+++ matcha-ranking-app/src/pages/seattle.jsx
@@ -1,5 +1,5 @@
 import "../App.css";
-import matcha from "../assets/matcha.png";
+import matcha from "matcha-ranking-app/src/assets/matcha-avni.png"
 
 export default function Seattle() {
   return (
