575b0f79d3aad3c7442055224aff477452b7711f
Avni Rao <64287021+avnirao@users.noreply.github.com>
2025-07-20 21:57:17 -0700

    Updates with more matcha!

--- matcha-ranking-app/src/pages/california.jsx
+++ matcha-ranking-app/src/pages/california.jsx
@@ -12,10 +12,13 @@ export default function california() {
       <p className="brand-heading-1">California (& beyond)</p>
       <p className="brand-heading-2">California</p>
       <p className="cafe-list">
-        <p>Cafe Maiko - San Jose, CA ★★★★★</p>
-        <p>Gathers Tea Bar - Chicago, IL ★★★★★</p>
-        <p>Molly Tea - Sunnyvale, CA ★★★☆☆ </p>
-        <p> La La Land Cafe - Los Angeles, CA ★★★★☆</p>
+        <p> Cafe Maiko - San Jose, CA ★★★★★ </p>
+        <p> Academic Coffee (Banana Cream Matcha) - Santa Clara, CA ★★★★★ </p>
+        <p> Gathers Tea Bar - Chicago, IL ★★★★★ </p>
+        <p> Molly Tea - Sunnyvale, CA ★★★☆☆ </p>
+        <p> Kiss of Matcha - San Francisco, CA ★★★★☆ </p>
+        <p> Dr.ink - San Jose, CA ★★★☆☆  </p>
+        <p> La La Land Cafe - Los Angeles, CA ★★★☆☆ </p>
         <p> LinkedIn Office (Mango Matcha) - San Francisco, CA ★★★☆☆ </p>
         <p> Hero Coffee & Bar - Chicago, IL ★☆☆☆☆ </p>
         </p>
